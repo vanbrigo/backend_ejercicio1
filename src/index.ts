28f3803a18102dc2e59e08f4d8f2850bdf1abbb0
Vanessa Brito <vanbrigo@MacBook-Air-de-Vanessa.local>
2023-10-18 13:38:44 +0200

    feat: create breakpoints for films

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,29 @@ import express from "express";
 
 const app = express()
 
+app.get('/films/:id',(req, res)=>{
+
+    return res.send('GET FILMS')
+})
+
+app.post('/films/:id',(req, res)=>{
+    return res.send('CREATE FILMS') 
+})
+
+app.put('/films/:id',(req, res)=>{
+    const filmId = req.params.id
+
+    console.log(req.params.id)
+    return res.send('UPDATE FILMS ' + filmId) 
+})
+
+app.delete('/films/:id',(req, res)=>{
+
+    const filmId = req.params.id
+
+    return res.send('DELETE FILMS '+ filmId) 
+})
+
 app.listen(3000, ()=>{
     console.log(`Server running`)
 })
